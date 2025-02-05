fabba235b5937c52244e1476a3b5b1467c7f2103
uthso297 <uthso119464@gmail.com>
2025-02-06 01:55:23 +0600

    completed social links

--- src/Home/Banner/Banner.jsx
+++ src/Home/Banner/Banner.jsx
@@ -8,14 +8,14 @@ const Banner = () => {
                     <h1 className='text-5xl lg:text-7xl'><span className='text-transparent bg-clip-text bg-gradient-to-t from-[#f4278d] to-[#8260b4]'>MERN STACK <br /> DEVELOPER</span></h1>
 
                     <div className='space-y-5'>
-                        <p className='text-white text-3xl'>I am Uthso - dedicated <span className='text-[#1d6ee6]'>web developer</span> with a passion for crafting visually appealing and responsive websites that deliver seamless user experiences.</p>
+                        <p className='text-white lg:text-3xl text-xl'>I am Uthso - dedicated <span className='text-[#1d6ee6]'>web developer</span> with a passion for crafting visually appealing and responsive websites that deliver seamless user experiences.</p>
 
                         <button className='btn btn-primary'>RESUME</button>
                     </div>
                 </div>
 
                 <div className='md:w-1/2 mt-5 md:mt-0 md:ml-10'>
-                    <img className="md:max-w-xl max-w-sm rounded-lg object-cover" src={bannerImg} alt="Banner image showcasing Uthso" />
+                    <img className="lg:max-w-xl max-w-sm rounded-lg object-cover" src={bannerImg} alt="Banner image showcasing Uthso" />
                 </div>
             </div>
 
--- src/Home/Home/Home.jsx
+++ src/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../Banner/Banner";
+import Social from "../Social/Social";
 
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Social></Social>
         </div>
     );
 };
