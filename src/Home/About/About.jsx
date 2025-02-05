9a0327a43883319bf25a37b8166afe8e7a7cd4c4
uthso297 <uthso119464@gmail.com>
2025-02-06 02:37:25 +0600

    designed about me

---
+++ src/Home/About/About.jsx
@@ -0,0 +1,24 @@
+
+const About = () => {
+    return (
+        <div className="bg-[#111723]">
+            <div className="md:max-w-7xl mx-auto p-4 md:pt-10 space-y-4">
+                <div>
+                    <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-t from-[#f4278d] to-[#8260b4]">About Me</h1>
+                </div>
+                <div>
+                    <p className="text-white">
+                        I began my programming journey with the C language. During my first semester at the university&apos;s Software Engineering Department, I recognized C as the &quot;mother language&quot; of programming. Mastering it would provide a solid foundation, making it easier to transition to other languages.
+
+                        After gaining proficiency in C and Data Structures and Algorithms (DSA), I shifted my focus to web development. Through Programming Hero, I explored the MERN stack, beginning with HTML, CSS, and JavaScript. I then expanded my skills to include React and backend technologies such as Node.js and Express. Over time, I became proficient in building full-stack MERN applications.
+                        <br />
+                        This journey was challenging, with many ups and downs, but I continuously pushed my limits to develop my skills and achieve my goal of becoming a capable developer. In addition to acquiring technical skills, I earned a CGPA of 3.88, demonstrating my dedication and commitment.
+                        In my free time, I enjoy exploring new technologies and dedicating time to my family, who have always been an important source of support.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
--- src/Home/Home/Home.jsx
+++ src/Home/Home/Home.jsx
@@ -1,3 +1,4 @@
+import About from "../About/About";
 import Banner from "../Banner/Banner";
 import Social from "../Social/Social";
 
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <Social></Social>
+            <About></About>
         </div>
     );
 };
